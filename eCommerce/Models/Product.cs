8f3f75fc9fce44d75fd101a7cb409d39720cbab0
Raymond Hampson III <rayhampson4@gmail.com>
2026-03-12 11:39:30 -0700

    Added Product class with properties for ProductId, Title, and Price. (#12)

---
+++ eCommerce/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.Models
+{
+    public class Product
+    {   
+        /// <summary> 
+        /// The unique identifier for the product.
+        /// </summary>
+        [Key]
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// The user facing title of the product.
+        /// </summary>
+        [StringLength(50, ErrorMessage = "Title cannot be more than 50 characters.")]
+        public required string Title { get; set; }
+
+        /// <summary>
+        /// The current sales price of the product in USD.
+        /// </summary>
+        [Range(0, 10_000)]
+        public double Price { get; set; }
+    }
+}
