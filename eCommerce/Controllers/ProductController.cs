fdd3a46e254b06dc5cb0c5a3860b711aa9e8dd19
Raymond Hampson III <rayhampson4@gmail.com>
2026-03-12 19:49:32 -0700

    Added ProductController and Create product view. (#15)

---
+++ eCommerce/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using eCommerce.Data;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly ProductDbContext _context;
+
+    public ProductController(ProductDbContext context)
+    {
+         _context = context;
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            // Add and save the product to the database
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+        return View(product);
+    }
+}
