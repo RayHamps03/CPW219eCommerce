4c5c1637272b4b07513f3d4928e442d03f8ac82e
Raymond Hampson III <rayhampson4@gmail.com>
2026-03-14 15:18:29 -0700

    Add documentation to ProductController

--- eCommerce/Controllers/ProductController.cs
+++ eCommerce/Controllers/ProductController.cs
@@ -7,12 +7,15 @@ namespace eCommerce.Controllers;
 
 public class ProductController : Controller
 {
+    // Dependency injection of the database context
     private readonly ProductDbContext _context;
 
     public ProductController(ProductDbContext context)
     {
          _context = context;
     }
+    
+   
     public async Task<IActionResult> Index()
     {
         List<Product> products = await _context.Products.ToListAsync();
@@ -25,6 +28,13 @@ public class ProductController : Controller
         return View();
     }
 
+    /// <summary>
+    /// Creates a new product in the database if the model state is valid.
+    /// </summary>
+    /// <param name="product">The product to be created.</param>
+    /// <returns>
+    /// A success message stored in temp data and redirect to Index view if model state is valid. 
+    /// </returns>
     [HttpPost]
     public async Task<IActionResult> Create(Product product)
     {
@@ -42,6 +52,13 @@ public class ProductController : Controller
         return View(product);
     }
 
+    /// <summary>
+    /// Displays an edit form for the specified product. 
+    /// </summary>
+    /// <param name="id">The ID of the product to be updated</param>
+    /// <returns>
+    /// A view of the product to be updated. If the product is not found, a 404 response is returned.
+    /// </returns>
     [HttpGet]
     public async Task<IActionResult> Edit(int id)
     {
@@ -57,6 +74,13 @@ public class ProductController : Controller
         return View(product);
     }
 
+    /// <summary>
+    /// Updates the specified product in the database if the model state is valid. 
+    /// </summary>
+    /// <param name="product">The product to be updated.</param>
+    /// <returns>
+    /// A success message stored in TempData and redirects the user to the Index view.
+    /// </returns>
     [HttpPost]
     public async Task<IActionResult> Edit(Product product)
     {
@@ -72,6 +96,14 @@ public class ProductController : Controller
     }
 
 
+    /// <summary>
+    /// Displays a confirmation page for deleting a specific product.
+    /// </summary>
+    /// <param name="id">The ID of the product to delete.</param>
+    /// <returns>
+    /// A view showing the product to be deleted, or a 404 response if the product is not found.
+    /// </returns>
+
     [HttpGet]
     public async Task<IActionResult> Delete(int id)
     { 
@@ -86,6 +118,14 @@ public class ProductController : Controller
 
         return View(product);
     }
+
+    /// <summary>
+    /// Permanently deletes the specified product after user confirmation.
+    /// </summary>
+    /// <param name="id">The ID of the product to delete.</param>
+    /// <returns>
+    /// A redirect to the product list. If the product does not exist, redirects without performing any action.
+    /// </returns>
 
     [ActionName("Delete")]
     [HttpPost]
