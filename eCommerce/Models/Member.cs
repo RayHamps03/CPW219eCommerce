62f83d8876891d64f0a037f95bb8462d2cc4a7de
Raymond Hampson III <rayhampson4@gmail.com>
2026-03-14 16:03:52 -0700

    Created Member Register view, RegistrationViewModel, and validation properties as well as navigation for the Member Register view.

--- eCommerce/Models/Member.cs
+++ eCommerce/Models/Member.cs
@@ -36,3 +36,38 @@ public class Member
     
     public DateOnly DateOfBirth { get; set; }
 }
+
+public class RegistrationViewModel
+{
+    /// <summary>
+    /// The user inputted username of the member
+    /// </summary>
+    [RegularExpression("^[a-zA-Z0-9]+$",
+        ErrorMessage = "Username must be alphanumeric.")]
+    [StringLength(25)]
+    public required string Username { get; set; }
+
+    /// <summary>
+    /// The email the user submitted for the member
+    /// </summary>
+    [DataType(DataType.EmailAddress)]
+    public required string Email { get; set; }
+
+    /// <summary>
+    /// The member's password
+    /// </summary>
+    [StringLength(50, MinimumLength = 6,
+        ErrorMessage = "Password must be between at least 6 to 50")]
+    [DataType(DataType.Password)]
+    public required string Password { get; set; }
+
+    [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+    [DataType(DataType.Password)]
+    public required string ConfirmPassword { get; set; }
+
+    /// <summary>
+    /// The date of birth of the member
+    /// </summary>
+    [DataType(DataType.Date)]
+    public DateOnly DateOfBirth { get; set; }
+}
