cccc01baf5e801314cb54412f57555ad2c773ef6
Raymond Hampson III <rayhampson4@gmail.com>
2026-03-14 15:21:50 -0700

    Added Member class for user representation.

---
+++ eCommerce/Models/Member.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.Models;
+
+public class Member
+{
+    /// <summary>
+    /// Unique identifier for the member
+    /// </summary>
+    [Key]
+    public int MemberId { get; set; }
+
+    /// <summary>
+    /// The user inputted username of the member
+    /// </summary>
+    public required string Username { get; set; }
+
+    /// <summary>
+    /// The email the user submitted for the member
+    /// </summary>
+    public required string Email { get; set; }
+
+    /// <summary>
+    /// The member's password
+    /// </summary>
+    public required string Password { get; set; }
+
+    /// <summary>
+    /// The date of birth of the member
+    /// </summary>
+    public DateOnly DateOfBirth { get; set; }
+}
