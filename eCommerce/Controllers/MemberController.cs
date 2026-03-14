e160f876db8dc3809e3dd955415e5372fb449003
Raymond Hampson III <rayhampson4@gmail.com>
2026-03-14 16:15:44 -0700

    Add registration handling for MemberController for proper user registration.

--- eCommerce/Controllers/MemberController.cs
+++ eCommerce/Controllers/MemberController.cs
@@ -1,11 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerce.Data;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerce.Controllers;
 
 public class MemberController : Controller
 {
+    private readonly ProductDbContext _context;
+
+    public MemberController(ProductDbContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Register()
     {
         return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegistrationViewModel register)
+    {
+        if (ModelState.IsValid)
+        {
+            // Map ViewModel to Member model tracked by db
+            Member newMember = new()
+            {
+                Username = register.Username,
+                Email = register.Email,
+                Password = register.Password,
+                DateOfBirth = register.DateOfBirth
+            };
+
+            _context.Members.Add(newMember);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+        return View(register);
     }
 }
