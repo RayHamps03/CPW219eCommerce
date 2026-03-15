56226e852d95ad6d65ad11ffe8ba61379ba016a7
Raymond Hampson III <rayhampson4@gmail.com>
2026-03-14 19:52:12 -0700

    Registration and login (#21)
    * Add documentation to ProductController
    
    * Added Member class for user representation.
    
    * Added Member class with properties and validation for int MemberId, string Username, string Email, string Password, and DateOnly DateOfBirth
    
    * Add unique constraints for Member Username and Email in DbContext
    
    * Add Member entity with migration.
    
    * Created Member Register view, RegistrationViewModel, and validation properties as well as navigation for the Member Register view.
    
    * Add registration handling for MemberController for proper user registration.
    
    * Added login view and LoginViewModel as well as a navigation link to the login view.
    
    * Add action that logs the user in if the username/email and password passed in matches a member's username/email and password in the database.
    
    * Added session state management. Includes idle timeout and cookie settings.
    
    * Add logout action for functionality and update navigation links that change when the user is logged in.

---
+++ eCommerce/Controllers/MemberController.cs
@@ -0,0 +1,81 @@
+﻿using eCommerce.Data;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Controllers;
+
+public class MemberController : Controller
+{
+    private readonly ProductDbContext _context;
+
+    public MemberController(ProductDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegistrationViewModel register)
+    {
+        if (ModelState.IsValid)
+        {
+            // Map ViewModel to Member model tracked by db
+            Member newMember = new()
+            {
+                Username = register.Username,
+                Email = register.Email,
+                Password = register.Password,
+                DateOfBirth = register.DateOfBirth
+            };
+
+            _context.Members.Add(newMember);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+        return View(register);
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel login)
+    {
+        if (ModelState.IsValid)
+        {
+            // Check if UsernameOrEmail and Password match a member in the database
+            Member? loggedInMember = await _context.Members
+                .Where(m => (m.Username == login.UsernameOrEmail || m.Email == login.UsernameOrEmail) 
+                    && m.Password == login.Password)
+                .SingleOrDefaultAsync();
+
+            if (loggedInMember == null)
+            {
+                ModelState.AddModelError(string.Empty, "Invalid username/email or password.");
+                return View(login);
+            }
+
+            // Log the user in
+            HttpContext.Session.SetString("Username", loggedInMember.Username);
+            HttpContext.Session.SetInt32("Id", loggedInMember.MemberId);
+
+            return RedirectToAction("Index", "Home");
+        }
+        return View(login);
+    }
+
+    public IActionResult Logout()
+    {
+        // Destroy current session
+        HttpContext.Session.Clear();
+        return RedirectToAction("Index", "Home");
+    }
+}
